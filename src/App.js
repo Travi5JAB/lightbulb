95b04884759a98a86fa09a6a72ba325343f9026f
Travi5JAB <travis841235@gmail.com>
2019-04-30 16:06:16 -0700

    random colors added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   return (
     <div>
         <LightBulb />
+        <LightBulb />
+
     </div>
   );
 }
--- src/lightbulb.js
+++ src/lightbulb.js
@@ -1,45 +1,59 @@
 import React, { Component } from 'react';
 
+
 class LightBulb extends Component{
     constructor(props){
         super(props)
         this.state = {
             light: "OFF",
-            turn: "ON",
+            backcolor: "#0000b3",
+            bulbcolor: "#808080b0",
+            textcolor: "white",
+            imagechange: "url('light-off.jpg')",
+            bodycolor: "#0000b3"
+
         }
     }
     toggeler = () => {
-        let { light, turn } = this.state
+        let arr = ["red", "green", "blue", "orange", "#ff00ff", "yellow", "purple", "black"]
+        let random = Math.floor(Math.random()*arr.length)
+        let { light, backcolor, bulbcolor, textcolor, imagechange, bodycolor} = this.state
         let newlight = light === "OFF" ? "ON" : "OFF";
-        let lightlabel = turn === "OFF" ? "ON" :"OFF";
+        let lightlabel = backcolor === "#0000b3" ? arr[random] :"#0000b3";
+        let bulbchange = bulbcolor === "#808080b0" ? arr[random] : "#808080b0"
+        let changetext = textcolor === "black" ? "white" : "black"
+        let changeimg = imagechange === "url('light-off.jpg')" ? "url('light-on.jpg')" : "url('light-off.jpg')"
+        let bodylight = bodycolor === "#0000b3" ? "yellow" :"#0000b3";
         this.setState({light: newlight})
-        this.setState({turn: lightlabel})
-        if(newlight === "ON"){
-            document.body.style.backgroundColor = "yellow";
-            document.body.style.color = "Black";
-            document.getElementById("lightbulbdiv").style.backgroundColor="white";
-            document.getElementById("button").style.backgroundImage = "url('light-on.jpg')";
+        this.setState({backcolor: lightlabel})
+        this.setState({bulbcolor: bulbchange})
+        this.setState({textcolor: changetext})
+        this.setState({imagechange: changeimg})
+        this.setState({bodycolor: bodylight})
 
-        }else{
-            document.body.style.backgroundColor = "#0000b3";
-            document.body.style.color = "White";
-            document.getElementById("lightbulbdiv").style.backgroundColor="grey";
-            document.getElementById("button").style.backgroundImage = "url('light-off.jpg')";
-        }
     }
     render() {
-        let { light, turn } = this.state
+        let { light, backcolor, bulbcolor, textcolor, imagechange, bodycolor } = this.state
         return(
-        <div>
-            <h3>Light: {light}</h3>
-            <button id = "button" onClick = {this.toggeler}></button>
-            <div id="lightbulbdiv"></div>
-            <div id= "lightbulbbase"></div>
-            <div id= "lightswitch"></div>
-            <div id= "buffer"></div>
+        <div id="buffer">
+            <div style = {{backgroundColor: bodycolor}}>
+                <h3 style = {{color: textcolor}}>Light: { light }</h3>
+                <button style = {{backgroundImage: imagechange}} id = "button" onClick = {this.toggeler}></button>
+            </div>
+            <div id="backUp" style = {{backgroundColor: bodycolor}}>
+                <div id="bulbandglow">
+                        <div id = "outerglow" style = {{backgroundColor: backcolor}}></div>
+                        <div class = "inner" id= "divCont" style = {{backgroundColor: backcolor}}></div>
+
+                        <div id= "wholebulb">
+                            <div class = "inner" id="lightbulbdiv" style = {{backgroundColor: bulbcolor}}></div>
+                            <div class = "inner" id= "lightbulbbase"></div>
+                        </div>
+
+
+                </div>
+            </div>
         </div>
-        // <a href="light-off.jpg"><img id= "imgOn" src= "light-on.jpg" ></img></a>
-        // <a href="light-on.jpg"><img id = "imgOff" src= "light-off.jpg"></img></a>
         )
     }
 }
